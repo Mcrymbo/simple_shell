1794356939b5f34675aa706b8f8361995b1daad4
Mcrymbo <croco2023@gmail.com>
2023-07-26 10:39:41 +0000

    non-interacive

--- shell_loop.c
+++ shell_loop.c
@@ -12,6 +12,8 @@ void shell_loop(shell_d *data)
 	loop = 1;
 	while (loop == 1)
 	{
+		if (isatty(0) == 0)
+			loop = 1;
 		write(STDIN_FILENO, "$ ", 2);
 		input = read_line(&line);
 		if (line != -1)
